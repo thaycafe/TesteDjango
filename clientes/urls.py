a515f54ab16f47b19adc01fbf969911e78778374
thaygneel <thaycic@hotmail.com>
2019-02-11 15:15:30 -0200

    Final 11.02

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,7 +1,13 @@
 
 from django.urls import path
 from .views import person_list
+from .views import person_new
+from .views import person_update
+from .views import person_delete
 
 urlpatterns = [
-    path('list/', person_list)
+    path('list/', person_list, name='person_list'),
+    path('new/', person_new, name='person_new'),
+    path('update/<int:id>/', person_update, name='person_update'),
+    path('delete/<int:id>/', person_delete, name='person_delete')
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,8 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Person
+from .forms import PersonForm
+
+acao = 'nada'
 
 
 def person_list(request):
     people = Person.objects.all()
     return render(request, 'person.html', {'seres': people})
+
+
 # Create your views here.
+
+
+def person_new(request):
+    form = PersonForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return redirect('person_list')
+    return render(request, 'person_form.html', {'form': form})
+
+
+def person_update(request, id):
+    acao = 'update'
+    person = get_object_or_404(Person, pk=id)
+    form = PersonForm(request.POST or None, request.FILES or None, instance=person)
+    if form.is_valid():
+        form.save()
+        return redirect('person_list')
+
+    return render(request, 'person_form.html', {'form': form, 'acao': acao})
+
+
+def person_delete(request, id):
+    acao='delete'
+    person = get_object_or_404(Person, pk=id)
+    form = PersonForm(request.POST or None, request.FILES or None, instance=person)
+
+    if request.method == 'POST':
+        person.delete()
+        return redirect('person_list')
+
+    return render(request, 'person_form.html', {'form': form, 'acao':acao})
